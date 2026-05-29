4850dbb62fb36da7b875ab14d6822b0c2d380ef1
Rishita Prajapati <rishita0337@gmail.com>
2026-05-29 20:32:45 +0530

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,18 +6,6 @@ component: Index,
 
 function Index() {
 return (
-<main
-style={{
-minHeight: "100vh",
-background: "#111",
-color: "white",
-display: "flex",
-justifyContent: "center",
-alignItems: "center",
-fontSize: "3rem",
-}}
->
 WEBSITE WORKS 🚀
-
 );
 }
