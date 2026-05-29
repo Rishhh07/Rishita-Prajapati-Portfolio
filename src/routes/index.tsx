0ddc48ac9d322507e48cdf590da28ee4d6f76167
Rishita Prajapati <rishita0337@gmail.com>
2026-05-29 20:21:33 +0530

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,23 +1,9 @@
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
-  component: Home,
+component: Home,
 });
 
 function Home() {
-  return (
-    <div
-      style={{
-        minHeight: "100vh",
-        background: "#111",
-        color: "white",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        fontSize: "2rem",
-      }}
-    >
-      Rishita Portfolio Works 🚀
-    </div>
-  );
+return Hello;
 }
